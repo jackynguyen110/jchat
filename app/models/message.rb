2625a7ed7b3065c56363195b79e35f72c90d102a
JackyNguyen <jacky.nguyen110@gmail.com>
2017-02-27 18:15:35 +0700

    fix bug

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ApplicationRecord
 	belongs_to :sender, class_name: 'User'
   	belongs_to :reciver, class_name: 'User'
 
-  	def self.countUnredMessage(reciver_id,sender_id)
+  	def self.countUnredMessage(sender_id, reciver_id)
   		return Message.where(sender_id: sender_id).where(reciver_id: reciver_id).where(read_at: nil).count
   	end
 
